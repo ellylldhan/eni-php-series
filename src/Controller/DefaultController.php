32bfec7a1be676b0814eff167e87f6948d4c94d3
ellylldhan <rinux.rinux@gmail.com>
2020-08-24 13:49:17 +0200

    2020-08-24 1349 : MOD2 DEMO2 - Afficher un fichier Twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController {
@@ -11,7 +12,8 @@ class DefaultController extends AbstractController {
      * @Route("/")
      */
     public function home() {
-        echo "Hello World !";
-        die();
+        // Afficher du html pur dans la page
+//        return new Response('Hello World!');
+        return $this->render("default/home.html.twig");
     }
 }
