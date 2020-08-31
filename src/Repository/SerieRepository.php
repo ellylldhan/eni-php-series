7df902d9ef3fc134f8d153e44359cbc54aa6c76b
ellylldhan <rinux.rinux@gmail.com>
2020-08-31 17:20:04 +0200

    2020-08-31 1720 : MOD7 DEMO3 - Doctrine : récupération entité associée

--- src/Repository/SerieRepository.php
+++ src/Repository/SerieRepository.php
@@ -18,76 +18,19 @@ class SerieRepository extends ServiceEntityRepository {
         parent::__construct($registry, Serie::class);
     }
 
-     /**
-      * @return Serie[] Returns an array of Serie objects
-      */
-    public function findGoodSeries() {
-
-        // Methode avec QueryBuilder
-//        $qb = $this->createQueryBuilder('s');
-//        $qb->andWhere('s.vote >= 8')
-//           ->andWhere('s.popularity >= 10')
-//           ->addOrderBy('s.vote', 'DESC');
-//        $qb->setMaxResults(30);
-//        $query = $qb->getQuery();
-//        return $query->getResult();
-
-        // Methode avec EntityManager
-        $em    = $this->getEntityManager();
-        $dql   = "SELECT s 
-                FROM App\Entity\Serie s 
-                WHERE s.vote >= 8 
-                AND s.popularity >= 10 
-                ORDER BY s.vote DESC";
-        $query = $em->createQuery($dql);
-        $query->setMaxResults(30);
-        $query->setFirstResult(0);
-
-        return $query->getResult();
-    }
-
     /**
      * @return Paginator Returns an array of Serie objects
      */
-    public function findTotalSeasons() {
+    public function findGoodSeries() {
         $qb = $this->createQueryBuilder('s');
         $qb->andWhere('s.vote >= 8')
            ->andWhere('s.popularity >= 10')
-            ->join('s.seasons', 's ')
+           ->join('s.seasons', 's ')
+           ->addSelect('seas')
            ->addOrderBy('s.vote', 'DESC');
         $qb->setMaxResults(30);
         $query = $qb->getQuery();
+
         return new Paginator($query);
-
-
-    // /**
-    //  * @return Serie[] Returns an array of Serie objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Serie
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
-
-
 }
